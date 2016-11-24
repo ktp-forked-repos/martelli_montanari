0acee5a701a50ae00e5cf1856b7a3a3d8fb6d023
Florent NOSARI <Florent NOSARI>
2016-11-24 20:22:08 +0100

    prepare application des règles

--- main.pl
+++ main.pl
@@ -1,3 +1,4 @@
+% Operateur ?=
 :- op(20,xfy,?=).
 
 % Prédicats d'affichage fournis
@@ -18,41 +19,60 @@ echo(_).
 
 % Regles 
 
-    % Renomage
-        % Vrai si les X et T sont des variables
-        regle(X?=T,rename) :- var(X), var(T).
+    % Conditions d'utilisation des règles'
 
-    % Simplification
-        % Vrai si X est une variable et T une constante
-        regle(X?=T,simplify) :- var(X), atomic(T).
+        % Renomage
+            % Vrai si les X et T sont des variables
+            regle(X?=T,rename) :- var(X), var(T).
 
-    % Développement
-        % Vrai si X est une variable, si T est composé et si X n'apparait pas dans T
-        regle(X?=T,expand) :- var(X), compound(T), occur_check(X,T).
+        % Simplification
+            % Vrai si X est une variable et T une constante
+            regle(X?=T,simplify) :- var(X), atomic(T).
 
-    % Teste d'ocurrence
-        % Vrai si X =/= T et si X apparait dans T
-        regle(X?=T,check) :- X\==T, \+occur_check(X,T).
+        % Développement
+            % Vrai si X est une variable, si T est composé et si X n'apparait pas dans T
+            regle(X?=T,expand) :- var(X), compound(T), occur_check(X,T).
 
-    % Orientation
-        % Vrai si T n'est pas une variable et si X en est une
-        regle(T?=X,orient) :- nonvar(T), var(X).
+        % Teste d'ocurrence
+            % Vrai si X =/= T et si X apparait dans T
+            regle(X?=T,check) :- X\==T, \+occur_check(X,T).
 
-    % Decomposition
-        % Vrai si on ne peut appliquer le règle de développement et si S et T sont deux fonctions de même sybole et de même arité
-        regle(S?=T,decompose) :- \+regle(S?=T,expand), !, functor(S,SF,SA), functor(T,TF,TA), SF==TF, SA==TA.
+        % Orientation
+            % Vrai si T n'est pas une variable et si X en est une
+            regle(T?=X,orient) :- nonvar(T), var(X).
 
-   % Conflit
-        % Vrai si S et T sont deux fonctions de différents symboles ou de différentes arités
-        regle(S?=T,clash) :- \+(functor(S,F,A)=functor(T,F,A)).
+        % Decomposition
+            % Vrai si on ne peut appliquer le règle de développement et si S et T sont deux fonctions de même sybole et de même arité
+            regle(S?=T,decompose) :- \+regle(S?=T,expand), !, functor(S,SF,SA), functor(T,TF,TA), SF==TF, SA==TA.
+
+        % Conflit
+                % Vrai si S et T sont deux fonctions de différents symboles ou de différentes arités
+                regle(S?=T,clash) :- \+(functor(S,F,A)=functor(T,F,A)).
+
+    % Application des règles : transforme le système d’équations P en le système d’équations Q par application de la règle de transformation R à l’équation E
+        % Renomage
+            reduit(rename,E,P,Q) :- . 
+        % Simplification
+            reduit(simplify,E,P,Q) :- .
+        % Développement
+            reduit(expand,E,P,Q) :- .
+        % Teste d'ocurrence
+            reduit(check,E,P,Q) :- .
+        % Orientation
+            reduit(orient,E,P,Q) :- .
+        % Decomposition
+            reduit(decompose,E,P,Q) :- .
+        % Conflit
+            reduit(clash,E,P,Q) :- .
+
 
         
         
+    % Helpers
+        % Fonction de teste d'ocurrence (V pas dans T ?)
 
-% Fonction de teste d'ocurrence (V pas dans T ?)
+            occur_check(V,T) :- term_variables(T, L), not_in(V,L), write(L).
 
-    occur_check(V,T) :- term_variables(T, L), not_in(V,L), write(L).
-
-    not_in(_X,[]).
-    not_in(X,[H|T]):- var(X), !, X\==H, not_in(X,T),!.
+            not_in(_X,[]).
+            not_in(X,[H|T]):- var(X), !, X\==H, not_in(X,T),!.
 
